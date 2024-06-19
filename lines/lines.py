a3c59d9205f40c2b4a7506d9346610e4742092d3
Jonathan Turnbull <jot84@aber.ac.uk>
2024-06-19 12:19:47 +0100

    json

--- lines/lines.py
+++ lines/lines.py
@@ -1,8 +1,9 @@
-import bagpy as bp
-import pandas as pd
+import json
 import os
-import matplotlib.pyplot as plt
 import tarfile as tf
+import matplotlib.pyplot as plt
+import pandas as pd
+import bagpy as bp
 
 # Drop columns
 drop = ["Time", "header.stamp.secs", "header.stamp.nsecs", "pose.orientation.x", 
@@ -17,35 +18,46 @@ finish_colormap = {3: "red", 1: "gray", 2: "red", 0: "green"}
 def is_zip_file(x: str): 
     return x.endswith(".bag.tar.gz")
 
+# Define a list to store extracted data
+extracted_data = []
+
 # Iterate through valid entries in the directory
 valid_entries = (entry for entry in os.scandir(rosbags_directory) if is_zip_file(entry.path))
-for entry in valid_entries: 
+for entry in valid_entries:
     tarfile = tf.open(entry.path, mode="r:gz")
     decompressed_path = None
-    for tarinfo in tarfile: 
+    for tarinfo in tarfile:
         decompressed_path = os.path.join(rosbags_directory, tarinfo.path)
     tarfile.extractall(path=rosbags_directory)
     tarfile.close()
-    
+
     bagfile = bp.bagreader(decompressed_path)
     if decompressed_path.endswith(".bag"):
-
         position = bagfile.message_by_topic("/turtlebot3/filtered/position")
         finish = bagfile.message_by_topic("/check_finished")
-    
+
         finish_df = pd.read_csv(finish)
         position_df = pd.read_csv(position)
         position_df = position_df.drop(drop, axis=1)
-    
-        x = position_df["pose.position.x"].values
-        y = position_df["pose.position.y"].values
-        finish_condition = finish_df.iloc[-1, 1]
 
-        if finish_condition == 0: 
+        x = position_df["pose.position.x"].values.tolist()
+        y = position_df["pose.position.y"].values.tolist()
+        finish_condition = int(finish_df.iloc[-1, 1])
+
+        data_entry = {
+            "file_name": entry.name,
+            "x": x,
+            "y": y,
+            "finish_condition": finish_condition
+        }
+
+        extracted_data.append(data_entry)
+
+        if finish_condition == 0:
             plt.plot(x, y, c="green")
         else:
             plt.plot(x, y, alpha=0.5)
-    
+
         plt.scatter(x[0], y[0], marker="o", c=finish_colormap[finish_condition])
         plt.scatter(x[-1], y[-1], marker="x", c=finish_colormap[finish_condition])
 
@@ -53,8 +65,13 @@ for entry in valid_entries:
         plt.plot([7, 7], [5, 3], c="black", lw=2)
         plt.plot([7, 7], [-3, -5], c="black", lw=2)
 
-# Save the figure
+# Save extracted_data to a JSON file
 experiment_name = os.path.basename(os.path.normpath(rosbags_directory))
+json_file_name = os.path.join(figures_directory, experiment_name + "_data.json")
+with open(json_file_name, 'w') as json_file:
+    json.dump(extracted_data, json_file, indent=4)
+
+# Save the figure
 file_name = os.path.join(figures_directory, experiment_name + ".png")
 plt.title(experiment_name)
 plt.savefig(file_name, dpi=600)
